2100f9e391b55c461e5d6c3a57697af164e1a568
Suyeon <ssy4784@naver.com>
2023-12-19 23:14:06 +0900

    Update : review python lecture practice

---
+++ 파이썬/실습코드/그래프_그리기.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel("Number")
+plt.show()
---
+++ 파이썬/실습코드/테스트.py
@@ -0,0 +1,2 @@
+print("Hello World")
+print("나는 Selena Stark 입니다.")
