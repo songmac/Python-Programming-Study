754e4d0f08429d50fb5167ebabd23959163a4c5a
Suyeon <ssy4784@naver.com>
2023-12-20 23:56:38 +0900

    Update : python example code : review

---
+++ 파이썬/예제/변수선언.py
@@ -0,0 +1,22 @@
+x = 10
+x + 10
+print(x)
+
+x = 20
+x = x + 5
+print(x)
+
+x, y, z = 30, 40, 50
+print(x, y, z)
+
+
+thisistestvalue = 100
+print(f"thisistestvalue = ", thisistestvalue)
+thisistestvalue += 100
+print(f"thisistestvalue += 100 -> thisistestvalue = ", thisistestvalue)
+thisistestvalue -= 100
+print(f"thisistestvalue -= 100 -> thisistestvalue = ", thisistestvalue)
+thisistestvalue *= 100
+print(f"thisistestvalue *= 100 -> thisistestvalue = ", thisistestvalue)
+thisistestvalue /= 100
+print(f"thisistestvalue /= 100 -> thisistestvalue = ", thisistestvalue)
---
+++ 파이썬/예제/자료형.py
@@ -0,0 +1,5 @@
+print(type(999))
+print(type(9.99))
+print(float(999))
+print(int(9.99))
+print(type(None))
