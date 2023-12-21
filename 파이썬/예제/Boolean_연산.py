0b39026b83ca207593d02bf58432f3ee96265388
Suyeon <ssy4784@naver.com>
2023-12-21 23:50:03 +0900

    Create : Boolean example

---
+++ 파이썬/예제/Boolean_연산.py
@@ -0,0 +1,29 @@
+# Boolean형
+print(type(True))
+
+
+# 비교연산
+print(3 > 1)
+print(3 >= 3)
+print(4 < 1)
+print(10 != 10)
+
+
+# 논리연산
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(False or True)
+print(True or False)
+print(False or False)
+
+print(not True)
+print(not False)
+
+
+# 비교, 논리 연산자 함께 사용
+print(5 > 10 and 20 == 20)
+print(5 == 5 or 10 != 10)
