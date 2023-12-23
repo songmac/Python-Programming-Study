0a042ea67b7b18d2a3a4595d43adc05e61409c36
Suyeon <ssy4784@naver.com>
2023-12-23 19:44:00 +0900

    Update : 서식지정자

--- 파이썬/예제/서식지정자.py
+++ 파이썬/예제/서식지정자.py
@@ -2,7 +2,7 @@ name = "Tom"
 
 print("I am", name, "!")
 print("I am "+name+" !")
-print("I am %s !" % name) # ,가 아닌 %
+print("I am %s !" % name) # ,가 아닌 % # 띄어쓰기 필수
 
 print("I am %10s !" % name)
 print("I am %-10s !" % name)
@@ -14,4 +14,5 @@ print("I met %s and %s." % (name2, name3))
 
 
 
-num1, num2 
+num1, num2, num3 = 3, 3.1245154, 4.56789
+print("num1 = %d, num2 = %f, num3(소수점이하) = %.2f, num3(자릿수맞추기) = %07.2f" % (num1, num2, num3, num3))
