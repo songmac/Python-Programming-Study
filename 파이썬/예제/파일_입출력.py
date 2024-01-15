84a7d2a39073e6b7f78edb443cccd0c4e14f7d8c
songmac <ssy4784@naver.com>
2024-01-16 00:35:22 +0900

    Update : example of file read, write

--- 파이썬/예제/파일_입출력.py
+++ 파이썬/예제/파일_입출력.py
@@ -8,3 +8,20 @@ file = open("file.txt", "r")
 text = file.read()
 print(text)
 file.close()
+
+
+
+# with : 자동으로 파일객체 닫기
+with open("file.txt","r") as file:
+    text = file.read()
+    print(text)
+
+
+
+# pickle 모듈을 이용하여 리스트를 파일에 저장
+import pickle
+    
+text = ["First File", "Second Line"]
+
+with open("data.p", "wb") as file:
+    pickle.dump(text, file)
