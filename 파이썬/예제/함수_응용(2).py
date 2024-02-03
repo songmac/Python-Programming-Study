dadc8f744b1be991cec93fc1ffe9447eb1165902
songmac <ssy4784@naver.com>
2024-02-03 20:43:48 +0900

    Create : example of function application case 2

--- 파이썬/예제/함수_응용(2).py
+++ 파이썬/예제/함수_응용(2).py
@@ -1,3 +1,19 @@
 # 키워드 인수 & 딕셔너리 언패킹
 
-def func
+def func(email, name):
+    print("이메일 : ", email, end="")
+    print(", 이름 : ", name)
+
+func(email = "aa@aa.com", name = "tom")
+
+x = {"email" : "bb@bb.com", "name" : "michael"}
+func(**x)
+
+
+
+# 가변 키워드인수
+def func(**kwargs):
+    print("이메일 : ", kwargs["email"], end="")
+    print(", 이름 : ", kwargs["name"])
+
+func(email = "cc@cc.com", name = "drake")
