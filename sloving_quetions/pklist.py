616127cff0e6e6a034a4fdb405a8798e4d0227b5
songmac <ssy4784@naver.com>
2024-02-26 21:20:20 +0900

    Create : practice of making a package file

---
+++ sloving_quetions/pklist.py
@@ -0,0 +1,14 @@
+import math as m
+
+def list_max(list1, list2, list3):
+    new_list = list1 + list2 + list3
+    print(max(new_list))
+
+def list_min(list1, list2, list3):
+    new_list = list1 + list2 + list3
+    print(min(new_list))
+
+
+def list_avg(list1, list2, list3):
+    new_list = list1 + list2 + list3
+    print(sum(new_list)/len(new_list))
