e646859dacb5fbf811b494dbee81acc173cb3820
songmac <ssy4784@naver.com>
2024-02-26 20:55:18 +0900

    Update : add method for import package with '__init__' python file

--- python_examples/20_모듈과패키지_패키지.py
+++ python_examples/20_모듈과패키지_패키지.py
@@ -15,3 +15,12 @@ from pkcalc.calc import name, add
 
 print(name)
 print(add(5,6))
+
+
+
+
+## 방법 3
+from .calc import add, sub
+"""
+패키지명으로 import하기 (__init__.py 파일 내용) 
+"""
---
+++ python_examples/pkcalc/__init__.py
@@ -0,0 +1,3 @@
+# 패키지명으로 import : __init__.py
+
+from .calc import add, sub
