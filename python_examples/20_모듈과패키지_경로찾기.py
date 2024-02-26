374e7fe62d8397f333544e771f5b3e889df1f533
songmac <ssy4784@naver.com>
2024-02-26 20:56:14 +0900

    Create : find paht for modules and packages

---
+++ python_examples/20_모듈과패키지_경로찾기.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.path)
+
+# 출력 결과 : site-packages 폴더에 pip로 설치한 패키지가 들어감
+"""['c:\\Users\\selena\\songmac\\Python-Programming-Study\\python_examples', 
+'C:\\Users\\selena\\anaconda3\\python311.zip', 
+'C:\\Users\\selena\\anaconda3\\DLLs', 
+'C:\\Users\\selena\\anaconda3\\Lib', 'C:\\Users\\selena\\anaconda3', 
+'C:\\Users\\selena\\anaconda3\\Lib\\site-packages', 
+'C:\\Users\\selena\\anaconda3\\Lib\\site-packages\\win32', 
+'C:\\Users\\selena\\anaconda3\\Lib\\site-packages\\win32\\lib', 
+'C:\\Users\\selena\\anaconda3\\Lib\\site-packages\\Pythonwin']"""
